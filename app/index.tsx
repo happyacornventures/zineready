2589878b2d86818925243ac6bb7c882a8470991e
Randy Brown <thatrandybrown@gmail.com>
2025-11-08 21:27:48 -0600

    create a basic text block

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { defaultConfig } from '@tamagui/config/v4'
 import { TamaguiProvider, createTamagui } from '@tamagui/core'
+import { Button, H1, Paragraph, YStack } from 'tamagui'
 
 // you usually export this from a tamagui.config.ts file
 const config = createTamagui(defaultConfig)
@@ -11,10 +12,27 @@ declare module '@tamagui/core' {
   interface TamaguiCustomConfig extends Conf {}
 }
 
-import { Button } from 'tamagui'
-
-export function Demo() {
-  return <Button theme="blue">Hello world</Button>
+export function HeroBanner() {
+  return (
+    <YStack
+      flex={1}
+      justify="center"
+      align="center"
+      padding="$4"
+      space="$3"
+      backgroundColor="$background"
+    >
+      <H1 color="$color">
+        Welcome to Your App
+      </H1>
+      <Paragraph size="$5" color="$color10">
+        This is a hero banner built with Tamagui. Start building your amazing application.
+      </Paragraph>
+      <Button theme="blue" size="$4">
+        Get Started
+      </Button>
+    </YStack>
+  )
 }
 
 export default () => {
